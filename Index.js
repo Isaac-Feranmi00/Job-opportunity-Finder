69ec70a742def308010df89592ed3f93a56efc27
Isaac Feranmi Paul <161623607+Isaac-Feranmi00@users.noreply.github.com>
2025-04-25 21:05:29 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('Form')
+const Username_input = document.getElementById('Username-input')
+const password_input = document.getElementById('Password-input')
+const Email_input = document.getElementById('Email-input')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()  
+})
